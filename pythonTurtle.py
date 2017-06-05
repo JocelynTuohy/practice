f0a251f7b285e0f3b7f9a40151d05a2437abbd5a
Jocelyn Tuohy <jocelyntuohy@hotmail.com>
2017-06-05 16:28:10 -0700

    completed python Turtle assignment (coding dojo)

--- pythonTurtle.py
+++ pythonTurtle.py
@@ -4,13 +4,22 @@
 import turtle
 # the distance we want the pointer to travel each time
 DIST = 100
+COLOR = ["orange","yellow","green","purple","black"]
 for x in range(0,6):
-    print "x", x
+    # print "x", x
+    turtle.pencolor(COLOR[x])
     for y in range(1,5):
-        print "y", y
+        # print "y", y
         # turn the pointer 90 degrees to the right
-        turtle.right(90)
+        turtle.right(40)
+        turtle.forward(50)
+        turtle.left(35)
+        turtle.backward(35)
+        turtle.left(50)
+        turtle.speed(1)
         # advance according to set distance
         turtle.forward(DIST)
+        turtle.speed(0)
+        turtle.circle(5)
     # add to set distance
     DIST += 20
