84cb557483c2009f12cefcec62ec5047e9efe787
Jocelyn Tuohy <jocelyntuohy@hotmail.com>
2017-06-05 14:51:32 -0700

    Completed checkerboard assignment and simplified compare arrays assignment

---
+++ checkerboard.py
@@ -0,0 +1,3 @@
+for twoLines in range(0,4):
+  print '* * * * '
+  print ' * * * *'
--- compareArrays.py
+++ compareArrays.py
@@ -2,8 +2,8 @@
 
 # Test Cases: toggle commenting to test each case
 # 
-list_one = [1,2,5,6,2]
-list_two = [1,2,5,6,2]
+# list_one = [1,2,5,6,2]
+# list_two = [1,2,5,6,2]
 # 
 # list_one = [1,2,5,6,5]
 # list_two = [1,2,5,6,5,3]
@@ -11,22 +11,10 @@ list_two = [1,2,5,6,2]
 # list_one = [1,2,5,6,5,16]
 # list_two = [1,2,5,6,5]
 # 
-# list_one = ['celery','carrots','bread','milk']
-# list_two = ['celery','carrots','bread','cream']
+list_one = ['celery','carrots','bread','milk']
+list_two = ['celery','carrots','bread','cream']
 
-same = True
-
-# test for same length
-if len(list_one) != len(list_two):
-  same = False
-else:
-  # loop through each member
-  for index in range(0,len(list_one)):
-  # compare each member
-    if list_one[index] != list_two[index]:
-      same = False
-      break
-if same:
+if list_one == list_two:
   print 'The lists are the same.'
 else:
   print 'The lists are not the same.'
